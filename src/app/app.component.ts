5fb63ef51975d25fbb657ce7deca32cb15b9ec55
Bob Rinehart <rinebob111185@gmail.com>
2019-07-19 17:55:26 -0700

    finished services.  started services assignment.  added active-users and inactive-users components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,57 @@
-import { Component } from '@angular/core';
+import { Component,   OnInit } from '@angular/core';
 
+// import { AccountsService } from './accounts.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  serverElements = [{type: 'server', name: 'TestserverDude', content: 'Freakin test dude'}];
+export class AppComponent implements OnInit {
+  // SERVICES ASSIGNMENT
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
 
-  	onServerAdded(serverData: {serverName: string, serverContent: string}) {
-		this.serverElements.push({
-			type: 'server',
-			name: serverData.serverName,
-			content: serverData.serverContent
-		});
-	}
+  ngOnInit() {}
 
-	onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-		this.serverElements.push({
-			type: 'blueprint',
-			name: blueprintData.serverName,
-			content: blueprintData.serverContent
-		});
-	}
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
+  // DATABINDING AND DIRECTIVES
+  // code for directives and data binding course sections
+  // serverElements = [{type: 'server', name: 'TestserverDude', content: 'Freakin test dude'}];
+
+  // 	onServerAdded(serverData: {serverName: string, serverContent: string}) {
+	// 	this.serverElements.push({
+	// 		type: 'server',
+	// 		name: serverData.serverName,
+	// 		content: serverData.serverContent
+	// 	});
+	// }
+
+	// onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+	// 	this.serverElements.push({
+	// 		type: 'blueprint',
+	// 		name: blueprintData.serverName,
+	// 		content: blueprintData.serverContent
+	// 	});
+  // }
+
+  // SERVICES 
+  // code for services course section 
+  // note: accounts array/data moved to AccountsService in Lec 107
+  // methods also moved to the service and deleted from here
+  // this is the new accountsService results holder
+    // accounts: {name: string, status: string}[] = [];
+    // constructor(private accountsService: AccountsService){}
+
+    // ngOnInit() {
+    //   this.accounts = this.accountsService.accounts;
+    // }
+
 }
