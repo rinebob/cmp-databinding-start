c5f72d1191ce9493ff84d24733370c9d968611e7
Bob Rinehart <rinebob111185@gmail.com>
2019-08-08 18:31:04 -0700

    through lec 148.  Added authGuard and canActivate and canDeactivate

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,28 @@
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router, CanActivateChild } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+
+	constructor(private authService: AuthService, private router: Router){}
+	canActivate(route: ActivatedRouteSnapshot,
+				state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+		return this.authService.isAuthenticated()
+			.then(
+				(authenticated: boolean) => {
+					if (authenticated) {
+						return true;
+					} else {
+						this.router.navigate(['/']);
+					}
+				}
+      )
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean{
+    return this.canActivate(route, state);
+  }
+}
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-server',
@@ -13,26 +14,50 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
+  // id: number;
 
   constructor(private serversService: ServersService,
-  			  private route: ActivatedRoute) { }
+          private route: ActivatedRoute,
+          private router: Router ) { }
 
   ngOnInit() {
-	console.log(this.route.snapshot.queryParams);
-	console.log(this.route.snapshot.fragment)
-  this.route.queryParams
-    .subscribe(
-      (queryParams: Params) => {
-        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false ;
-      });
-	this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    this.route.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false ;
+        });
+    this.route.fragment.subscribe();
+    let id = +this.route.snapshot.params['id'];
+    console.log('init id: ',id)
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          console.log('subscribed id: ',id)
+          id = +params['id']
+        }
+      )
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
+
+  canDeactivate():  Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit){
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved ) {
+      console.log('dont do it!');
+      return confirm('Dude you sure you want to discard yer effin changes???');
+    } else {
+      return true;
+    }
+  };
 
 }
